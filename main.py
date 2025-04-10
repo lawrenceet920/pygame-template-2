81f6fa838b6eab8774890601a80e098e48256031
lawrenceet920 <153318146+lawrenceet920@users.noreply.github.com>
2025-04-10 12:42:02 -0400

    minor changes to format
    New colors
    More comments
    Changed Whitespace
    More Clearly defined workspace for project outside of baseline/template

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import pygame
 import sys
 import config
 
+# Generic Functions
 def init_game():
     pygame.init()
     screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT))
     pygame.display.set_caption(config.TITLE)
     return screen
-
 def handle_events():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -24,14 +24,22 @@ def main():
     screen = init_game()
     clock = pygame.time.Clock()
     running = True
+    # On Startup
+
     while running:
         running = handle_events()
         screen.fill(config.WHITE)
-        pygame.display.flip()
+        # While Running
 
-        # Limit clock to FPS
+
+        # Limit clock to FPS & Update Screen
+        pygame.display.flip()
         clock.tick(config.FPS)
     pygame.quit()
     sys.exit()
+
+# Other Functions
+    
+# Startup
 if __name__ == '__main__':
     main()
